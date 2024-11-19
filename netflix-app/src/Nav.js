f1256bfa3b87220af49716a816d8af1e516f5e27
FatlumG <167577293+FatlumG@users.noreply.github.com>
2024-11-19 21:49:31 +0100

    Set up firebase and created navbar

---
+++ netflix-app/src/Nav.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+
+    return () => window.removeEventListener("scroll", transitionNavbar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <div className="nav-contents">
+        <img
+          className="nav-logo"
+          src="https://loodibee.com/wp-content/uploads/Netflix-logo.png"
+          alt="Netflix"
+        />
+
+        <img
+          className="nav-avatar"
+          src="https://e7.pngegg.com/pngimages/980/304/png-clipart-computer-icons-user-profile-avatar-heroes-silhouette-thumbnail.png"
+          alt="Profile photo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ netflix-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYmqeU6JCI8coRprAy3fsK7gbj8Vctvb8",
+  authDomain: "netflix-clone-app-c9e26.firebaseapp.com",
+  projectId: "netflix-clone-app-c9e26",
+  storageBucket: "netflix-clone-app-c9e26.firebasestorage.app",
+  messagingSenderId: "834206207423",
+  appId: "1:834206207423:web:1d0af24b4c6ed80d7de50a",
+};
