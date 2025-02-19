ca111bf9af1c1eecb32bfd58f98c7b6e27e896f7
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-02-19 18:13:41 +0100

    Deleted some unnecessary files

--- netflix-app/src/pages/HomePage.jsx
+++ netflix-app/src/pages/HomePage.jsx
@@ -22,8 +22,8 @@ function HomePage() {
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      {/* <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> */}
+      {/* <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
     </div>
   );
 }
